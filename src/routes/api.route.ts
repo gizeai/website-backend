09d4d8829ab0a59664d7cb6eeebcecd17b655baf
oKauaDev <kauacomtil021@gmail.com>
2025-04-11 08:24:13 -0300

    :sparkles: feat(database): Renomeado container do banco de dados e adicionado Zod como dependência.

--- src/routes/api.route.ts
+++ src/routes/api.route.ts
@@ -21,8 +21,8 @@ apiRouter.get("/", async (_req, res) => {
     const maxConnections: { setting: string }[] =
       await prisma.$queryRaw`SELECT setting FROM pg_settings WHERE name = 'max_connections'`;
 
-    const openConnections: { count: string }[] =
-      await prisma.$queryRaw`SELECT COUNT(*) FROM pg_stat_activity`;
+    const openConnections: { count: string }[] = await prisma.$queryRaw`SELECT COUNT(*)
+FROM pg_stat_activity WHERE backend_type = 'client backend';`;
 
     const dbVersion: { version: string }[] = await prisma.$queryRaw`SELECT version()`;
 
