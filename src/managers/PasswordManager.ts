9d7e38cedb33e4a18796159c71e55a4c754ca9f7
oKauaDev <kauacomtil021@gmail.com>
2025-04-11 11:19:09 -0300

    :rocket: add(managers/PasswordManager): Implementado hashing e validação de senhas com bcrypt.

--- src/managers/PasswordManager.ts
+++ src/managers/PasswordManager.ts
@@ -1,9 +1,14 @@
+import bcrypt from "bcrypt";
+
 export default class PasswordManager {
   public static hashPassword(password: string): string {
-    return password;
+    const saltRounds = 10;
+    const salt = bcrypt.genSaltSync(saltRounds);
+    const hash = bcrypt.hashSync(password, salt);
+    return hash;
   }
 
   public static comparePassword(password: string, hash: string): boolean {
-    return password === hash;
+    return bcrypt.compareSync(password, hash);
   }
 }
