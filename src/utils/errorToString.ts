2fbdc201654af9f55d689362fc2ba999047a2357
oKauaDev <kauacomtil021@gmail.com>
2025-04-29 09:21:49 -0300

    :sparkles: feat(utils/errorToString): Melhorada função de conversão de erros para incluir novos casos.

--- src/utils/errorToString.ts
+++ src/utils/errorToString.ts
@@ -37,5 +37,13 @@ export default function errorToString(error: unknown): string {
     return error.message;
   }
 
+  if (error && typeof error === "object" && "message" in error) {
+    return String(error.message);
+  }
+
+  if (error && typeof error === "object" && "error" in error) {
+    return String(error.error);
+  }
+
   return String(error);
 }
