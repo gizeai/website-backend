bbde9d062120d4e0ef8bfc4bb080c678baa7420f
oKauaDev <kauacomtil021@gmail.com>
2025-04-30 15:32:51 -0300

    :recycle: refactor: Removidos diretórios 'uploads' das configurações de ignorar.

--- src/utils/createDirs.ts
+++ src/utils/createDirs.ts
@@ -13,7 +13,3 @@ if (!fs.existsSync("./__cache__/app.log")) {
 if (!fs.existsSync("./__cache__/quotes.json")) {
   fs.writeFileSync("./__cache__/quotes.json", "{}");
 }
-
-if (!fs.existsSync("./uploads")) {
-  fs.mkdirSync("./uploads");
-}
