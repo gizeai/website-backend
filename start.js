48ef71afcefa17a4ff67eabf843e56ecfcb4b681
oKauaDev <kauacomtil021@gmail.com>
2025-04-30 13:56:00 -0300

    :recycle: refactor(start.js): Atualizado caminho base para incluir a pasta 'src' dentro de 'dist'.

--- start.js
+++ start.js
@@ -3,7 +3,7 @@
 const tsConfigPaths = require("tsconfig-paths");
 const path = require("path");
 
-const baseUrl = path.resolve(__dirname, "dist");
+const baseUrl = path.resolve(__dirname, path.join("dist", "src"));
 const config = require("./tsconfig.json");
 
 tsConfigPaths.register({
